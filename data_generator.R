9a0b0c2f20c52ececf77dfece928c2f62e7cf2a2
Aidan Neher <atm.neher@gmail.com>
2023-11-07 16:34:35 -0600

    Merge Tiankai and Aidan's data generating processes

--- data_generator.R
+++ data_generator.R
@@ -1,64 +1,115 @@
-n<-100
-N<-2*n
+# NHANES proportion (https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0255583)
 
-#Create Covariates
-#Gender
-Male <- rbinom(N, 1, .5) #just have genders be 50/50 for now
-#Age
-AgeDiff <- runif(1, -10, 10)#size of age difference between trial and target pops
-AgeTrial <- rpois(n, 50+AgeDiff*.5) #create vector of trial ages (integers, centered at 1/2 dist away from 50)
-AgeTarget <- rpois(n, 50-AgeDiff*.5)
-Age <- c(AgeTrial, AgeTarget) #vector with n trial then n target ages
-#Race
-RaceTrial <-sample(c("Black", "White", "Asian", "Hispanic", "Other"), n, replace=TRUE, prob=c(0.1, 0.5, .2, .15, .05))
-RaceTarget <-sample(c("Black", "White", "Asian", "Hispanic", "Other"), n, replace=TRUE, prob=c(0.2, 0.4, .1, .25, .05))
-Race <- c(RaceTrial, RaceTarget)
-#BMI
-#make this some function like: h(age, gender)+random term
-#for now, just make normal
-BMI<-rnorm(N, 24, 5)
+library(tidyverse)
 
-## -- Differentially Specified Variable
+set.seed(343)
 
-# Set parameters
+# Parameters for sample sizes
+N = 200
+N_Trial = 100
+N_Target = N - N_Trial
+
+# Parameters for differential specification
 k <- 0.05
 l <- 0.5
 p <- 0.5
 
-sample_V <- function(n=100, k=0.05, l=0.5, seed=1) {
-  
-  set.seed(seed)
-  
-  # Generate Latent Variable
-  UTrial<- rnorm(n)
-  UTarget<- rnorm(n) # TODO are we fine with standard normal latent variables? Any normal variable can be centered and scaled... 
-  
-  # Cut Latent Variable
-  Trial_cut_less <- rbinom(1, 1, prob = 0.5) # Binary indicator for Trial cut point less than Target cut point
-  a <- l - k/2
-  b <- l + k/2
-  a_thresh <- qnorm(a)
-  b_thresh <- qnorm(b)
-  if (Trial_cut_less==1) {
-    VTrial <- ifelse(UTrial < a_thresh, "Low", "High")
-    VTarget <- ifelse(UTarget < b_thresh, "Low", "High")
-  } else {
-    VTrial <- ifelse(UTrial < b_thresh, "Low", "High")
-    VTarget <- ifelse(UTarget < a_thresh, "Low", "High")
-  }
-  
-  V = factor(c(VTrial, VTarget), levels = c("Low", "High"))
-  
-  V_df <- data.frame(population = factor(c(rep("Trial", n), rep("Target", n))), V)
-            
-  return(V_df)
+##### Gender ##### 
+p_male = 0.396 # p_male = 0.396 from NHANES
+Male_trial <- rbinom(N_Trial, 1, p_male)
+Male_target <- rbinom(N_Target, 1, p_male)
+
+Male = c(Male_trial, Male_target)
+
+##### Age ##### 
+# Trial
+alpha <- 7.5
+beta <- 5.4
+Sim_age_Trial <- rbeta(N_Trial, alpha, beta)
+
+# Adjust the simulated age data to your desired age range
+min_age <- 15 
+max_age <- 80   
+Sim_age_Trial <- min_age + Sim_age_Trial * (max_age - min_age)
+
+# Target
+alpha <- 6
+beta <- 5
+Sim_age_Target <- rbeta(N_Target, alpha, beta)
+
+# Adjust the simulated age data to your desired age range
+min_age <- 18   
+max_age <- 75   
+Sim_age_Target <- min_age + Sim_age_Target * (max_age - min_age)
+
+Age = c(Sim_age_Trial, Sim_age_Target)
+
+#Race
+Race_Trial <-sample(c("Non-Hispanic White", "Non-Hispanic Black", "Hispanic", "Asian", "Other"), 
+                    N_Trial, replace=TRUE, prob=c(0.508, 0.234, 0.205, 0.032, 0.021)) # From NHANES
+Race_Target <-sample(c("Non-Hispanic White", "Non-Hispanic Black", "Hispanic", "Asian", "Other"), 
+                     N_Target, replace=TRUE, prob=c(0.544, 0.253, 0.16, 0.03, 0.013)) # From CVD study 
+Race <- as.factor(c(Race_Trial, Race_Target))
+
+# Use model.matrix to create one-hot encoded vectors
+# The "-1" removes the intercept term
+Race_one_hot_encoded <- model.matrix(~ Race - 1, data.frame(Race)) %>% as.data.frame()
+
+# Rename the columns for clarity (optional)
+colnames(Race_one_hot_encoded) <- levels(Race)
+
+#BMI
+slope_age <- 0.6 # From NHANES
+slope_sex <- -0.425 # From NHANES
+BMI <- slope_age * Age + slope_sex * Male  + rnorm(N, mean = 0, sd = 6.5) # Males on average have lower BMI
+
+# Generate Latent Variable
+UTrial <- rnorm(N_Trial)
+UTarget <- rnorm(N_Target) 
+
+# Cut Latent Variable
+Trial_cut_less <- rbinom(1, 1, prob = 0.5) # Binary indicator for Trial cut point less than Target cut point
+a <- l - k/2
+b <- l + k/2
+a_thresh <- qnorm(a)
+b_thresh <- qnorm(b)
+if (Trial_cut_less==1) {
+  VTrial <- ifelse(UTrial < a_thresh, "Low", "High")
+  VTarget <- ifelse(UTarget < b_thresh, "Low", "High")
+} else {
+  VTrial <- ifelse(UTrial < b_thresh, "Low", "High")
+  VTarget <- ifelse(UTarget < a_thresh, "Low", "High")
 }
 
-# Create other variables
-ATrial<-rbinom(n, 1, .5)
-A <- c(ATrial, rep(NA, n)) #1 if treat, 0 if control (only for those in trial!)
-S <- c(rep(1,n), rep(0,n))#indicator variable for trial
-Y0 <- 3*Male -.2*Age + 4*BMI+as.numeric(V=="High")*3+as.numeric(Race=="Black")*2 +rnorm(N, 100,sd=50) 
-Y1 <- 3*Male -.2*Age + 4*BMI+as.numeric(V=="High")*3+as.numeric(Race=="Black")*2 +rnorm(N, 100,sd=50) +30
-Y  <- ifelse(A==1, Y1,Y0) 
+V = factor(c(VTrial, VTarget), levels = c("Low", "High"))
+
+# Create treatment and study indicator variables
+ATrial <- rbinom(N_Trial, 1, .5)
+A <- c(ATrial, rep(NA, N_Target)) # 1 if treat, 0 if control (only for those in trial!)
+S <- c(rep(1, N_Trial), rep(0, N_Target)) # indicator variable for trial
+
+# Define potential outcomes
+ATE <- 30
+beta_0 <- 100
+epsilon <- 50
+beta_race <- matrix(5:1, ncol = 1)
+beta_male <- 3
+beta_age <- -.2
+beta_bmi <- 1
+beta_V <- 3
+
+effect_race <- as.matrix(Race_one_hot_encoded) %*% beta_race %>% 
+  as.vector()
+
+Y0 <- beta_male * Male + beta_age * Age + beta_bmi * BMI + beta_V * as.numeric(V=="High") + 
+  effect_race + rnorm(N, beta_0, sd = epsilon) 
+Y1 <- beta_male * Male + beta_age * Age + beta_bmi * BMI + beta_V * as.numeric(V=="High") + 
+  effect_race + rnorm(N, beta_0, sd = epsilon) + ATE
+Y <- ifelse(A==1, Y1, Y0) # Y is NA for subjects in the target population
+
+mean(Y0)
+mean(Y1)
+mean(Y, na.rm = TRUE)
+
 data <- data.frame(Male, Age, Race, BMI, V, A, S, Y0, Y1, Y)
+
